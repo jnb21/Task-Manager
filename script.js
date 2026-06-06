2df710b8d51edc73f95f6628eb15da284f0f13c1
Jordan <bikong@mac.home>
2026-06-06 15:55:45 -0400

    First modifications

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
 
 addTaskBtn.addEventListener("click", () => {
-    console.log("clicked");
 
+    // task title
     const taskName = prompt("Enter task:");
 
-    if (!taskName) return;
+    if (!taskName || taskName.trim() === "") return;
 
+    // task priority
+    const priority = prompt(
+        "Choose priority: Low, Medium, or High"
+    );
+
+    if (!priority) return;
+
+    // create task card
     const taskCard = document.createElement("div");
+
     taskCard.classList.add("task-card");
 
-    taskCard.innerHTML = `<h3>${taskName}</h3>`;
+    // add priority color class
+    taskCard.classList.add(priority.toLowerCase());
+
+    taskCard.innerHTML = `
+        <h3>${taskName}</h3>
+        <p>Priority: ${priority}</p>
+    `;
 
     taskList.appendChild(taskCard);
 });
